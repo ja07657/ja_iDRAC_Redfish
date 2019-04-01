fcfcaea2b683bdcfcba38b495da30a18829f2225
Jason Amato <amato_jason@yahoo.com>
2019-04-01 16:27:45 -0400

    apr1

--- Redfish Python/main.sh
+++ Redfish Python/main.sh
@@ -6,11 +6,12 @@ echo "Enter user for iDRAC"
 read -s DRACUSER
 echo "Enter password for iDRAC"
 read -s DRACPASS
-echo "Enter name of python redfish script"
-read PYTHON
-#call python script
+#echo "Enter name of python redfish script"
+#read PYTHON
+#
 for DRACIP in `cat ipfile`
 do
 	echo $ip" "$PYTHON
-	python $PYTHON -ip $DRACIP -u $DRACUSER -p $DRACPASS
+	#python $PYTHON -ip $DRACIP -u $DRACUSER -p $DRACPASS
+	python $1 -ip $DRACIP -u $DRACUSER -p $DRACPASS
 done
