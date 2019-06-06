dd43431f378e971deecc3fd01e4bdbe4dd516c5c
Jason Amato <amato_jason@yahoo.com>
2019-06-06 10:12:45 -0400

    vm2

--- RedfishPython/vm_main.sh
+++ RedfishPython/vm_main.sh
@@ -13,7 +13,7 @@ do
 	#python $1 -ip $DRACIP -u $DRACUSER -p $DRACPASS
 	python InstallFromRepositoryREDFISH.py -g y -ip $DRACIP -u $DRACUSER -p $DRACPASS ## get firmware inventory ##
  	python InsertEjectVirtualMediaREDFISH.py -o2 -d1 -ip $DRACIP -u $DRACUSER -p $DRACPASS ##eject##
- 	python InsertEjectVirtualMediaREDFISH.py -o1 -d1 -i http://10.40.196.209/iso/Polaris_14G_June2019.iso -ip $DRACIP -u $DRACUSER -p $DRACPASS ##mount##
+ 	python InsertEjectVirtualMediaREDFISH.py -o1 -d1 -i http://10.40.196.209/iso/Polaris_BIOS_Jun2019.iso -ip $DRACIP -u $DRACUSER -p $DRACPASS ##mount##
  	python InsertEjectVirtualMediaREDFISH.py -cy -ip $DRACIP -u $DRACUSER -p $DRACPASS ##status##
  	python SetNextOneTimeBootVirtualMediaDeviceOemREDFISH.py -d1 -ry -ip $DRACIP -u $DRACUSER -p $DRACPASS ## set onetime boot device to vm  and reboot y##
 
