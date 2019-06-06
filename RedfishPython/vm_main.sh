e8cc72bc755faa67fc5d937de0c37c0acaf05a8a
Jason Amato <amato_jason@yahoo.com>
2019-06-06 09:49:06 -0400

    vm2

--- RedfishPython/vm_main.sh
+++ RedfishPython/vm_main.sh
@@ -11,7 +11,7 @@ for DRACIP in `cat ipfile`
 do
 	echo "Running on..."$DRACIP
 	#python $1 -ip $DRACIP -u $DRACUSER -p $DRACPASS
-	python InstlalFromRepositoryREDFISH.py -g y -ip $DRACIP -u $DRACUSER -p $DRACPASS ## get firmware inventory ##
+	python InstallFromRepositoryREDFISH.py -g y -ip $DRACIP -u $DRACUSER -p $DRACPASS ## get firmware inventory ##
  	python InsertEjectVirtualMediaREDFISH.py -o2 -d1 -ip $DRACIP -u $DRACUSER -p $DRACPASS ##eject##
  	python InsertEjectVirtualMediaREDFISH.py -o1 -d1 -i http://10.40.196.209/iso/Polaris_14G_June2019.iso -ip $DRACIP -u $DRACUSER -p $DRACPASS ##mount##
  	python InsertEjectVirtualMediaREDFISH.py -cy -ip $DRACIP -u $DRACUSER -p $DRACPASS ##status##
