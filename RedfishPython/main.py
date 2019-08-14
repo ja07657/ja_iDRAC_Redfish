c51cae23c4cd4d6cd0acff8ffb1add0f919c4ed8
U-AMERICAS\Jason_Amato <Jason_Amato@W109V42PQ2.aus.amer.dell.com>
2019-08-14 08:27:09 -0400

    add main.py

---
+++ RedfishPython/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# main.py jason amato
+#000000000000000000000000000000
+#-------------------------------
+#set all attributes the script uses in this var - PUT A SPACE before the -  !!!!!
+#ie GetSystemHWInventoryREDFISH_ja.py -a y    , then set otherattribs to "-a y"
+otherattribs = " -a y"
+#-------------------------------
+
+import os
+
+iplist = list()
+#
+f = open('ipfile')
+for line in f :
+  ip = line.rstrip()
+  ip = str(ip)
+  iplist.append(ip)  
+
+iplist = sorted(iplist)
+print(f'Running on: { iplist }')
+
+prog1 = input('Enter the program to run: ')
+user1 = input('iDRAC Username: ')
+pass1 = input('iDRAC Password: ')    
+
+check1 = input('Are you sure you want to run this? Type Y to continue...')
+if check1 == 'Y' :
+   for ip1 in iplist : 
+      print(f'running on: { ip }')
+      cmdline = 'python ' + prog1 + ' -ip ' + ip1 + ' -u ' + user1 + ' -p ' + pass1 + otherattribs
+      print(f'running: { cmdline }')
+      os.system(cmdline)
+
+print('Done.')
+      
+  
